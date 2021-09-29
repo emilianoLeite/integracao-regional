4bc40174d448296f9e504a652b7d2e5a5d151bb4
Emiliano Leite <emiliano.leite@creditas.com.br>
2021-09-28 22:30:07 -0300

    Begin form reducer

--- pages/quiz1/index.js
+++ pages/quiz1/index.js
@@ -1,12 +1,46 @@
 
 import Question from './question';
+import { useReducer } from 'react';
+
+const correctAnswers = {
+  question0: 0,
+  question1: 1,
+  question2: 2,
+  question3: 3,
+
+}
+const reducerInitialState = {
+  currentQuestion: 0,
+  question0: null,
+  question1: null,
+  question2: null,
+  question3: null,
+}
+
+function formReducer(state, action) {
+  switch (action.type) {
+    case 'answerSelected':
+      console.log(state, action);
+      return state;
+    case 'previousButtonClicked':
+      console.log(state, action);
+      return {...state, currentQuestion: currentQuestion - 1};
+    case 'nextButtonClicked':
+      console.log(state, action);
+      return {...state, currentQuestion: currentQuestion + 1};
+    default:
+      return state;
+  }
+}
 
 export default function Quiz1() {
+  const [state, dispatch] = useReducer(formReducer, reducerInitialState)
+  
   return (
   <div className="container">
     <div className="content-centered">
       <div className="w-layout-grid testimonial-grid">
-        <Question/>
+        <Question formState={state} dispatcher={dispatch}/>
     
       </div>
     </div>
--- pages/quiz1/question.js
+++ pages/quiz1/question.js
@@ -1,53 +1,59 @@
+import { useState } from 'react';
+import AnswerButton from './answer-button';
 import styles from './quiz1.module.css';
 
-export default function Question() {
+export default function Question({ formState, dispatcher }) {
+  const [selectedOption, setSelectedOption] = useState(null)
+  
+  const handleClick = (index) => {
+    setSelectedOption(index);
+    dispatcher({ type: 'answerSelected', payload: index })
+    console.log(`selecetd`, selectedOption);
+  }
+  
   return (
     <div className="testimonial-card">
-        <div className="testimonial-content">
-          <h4> - Questão 1 - </h4>
-          <p>I have just launched my first digital product and it’s all thanks to you. Everyone love it! Already reached my sales goal in 5 hours. Thank you!</p>
-          <form>
-          <a href="#"  className={styles.answerButton}>
-            <div className="button-back-bcg"></div>
-            <div className="button-top-bcg"></div>
-            <div className="button-text">
-              <input 
-                type="radio" id="quiz1-answer1" name="quiz1-answer1" value="quiz1-answer1" 
-                
-              />
-              <label className={styles.answerLabel} htmlFor="quiz1-answer1">Teste</label>
-            </div>
-          </a>
-          <a href="#" 
-          className={styles.answerButton}>
-            <div className="button-back-bcg"></div>
-            <div className="button-top-bcg"></div>
-            <div className="button-text">
-              <input 
-                type="radio" id="quiz1-answer2" name="quiz1-answer1" value="quiz1-answer2"      
-                
-              />
-              <label className={styles.answerLabel} htmlFor="quiz1-answer2">Teste</label>
-            </div>
-          </a>
+      <div className="testimonial-content">
+        <h4> - Questão 1 - </h4>
+        
+        <p>
+          I have just launched my first digital product and it’s all thanks to you. Everyone love it! Already reached my sales goal in 5 hours. Thank you!
+        </p>
+        
+        <form>
+          <AnswerButton 
+            formName={"quiz1"} answerLabel={"Teste"} answerIndex={0}
+            onClick={() => handleClick(0)}
+            selected={selectedOption === 0}    
+          />
+          <AnswerButton 
+            formName={"quiz1"} answerLabel={"Teste2"} answerIndex={1}
+            onClick={() => handleClick(1)}
+            selected={selectedOption === 1}
+          />
+          <AnswerButton 
+            formName={"quiz1"} answerLabel={"Test3"} answerIndex={2}
+            onClick={() => handleClick(2)}
+            selected={selectedOption === 2}
+          />
+        
+          
           <div className={styles.footer}>
             <button type="button" className={styles.secondaryButton} data-bcup-haslogintext="no">
               <img src="https://assets.website-files.com/60d0b9d684d42a0424dfb8c5/60d0ba000e4b763ca38903e4_btn_arrow.svg" loading="lazy" alt="" style={{
                 "transform": "rotate(180deg) translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", 
                 transformStyle: "preserve-3d"
               }}/>
-              </button>
+            </button>
             <button type="button" className={`${styles.submitButton} ${styles.wButton} `}>
               <img src="https://assets.website-files.com/60d0b9d684d42a0424dfb8c5/60d0ba000e4b763ca38903e4_btn_arrow.svg" loading="lazy" alt="" style={{
                 "transform": "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)", 
                 transformStyle: "preserve-3d"
               }}/>
-              
-              </button>
+            </button>
           </div>
-      </form>
-        </div>
-
+        </form>
+      </div>
     </div>
   )
 }
