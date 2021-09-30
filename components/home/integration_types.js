a989c072cb96bb0fceea0e1eb026857f3df274fa
Emiliano Leite <emiliano.leite@creditas.com.br>
2021-09-30 10:40:30 -0300

    Implement home/integration_types links

--- components/home/integration_types.js
+++ components/home/integration_types.js
@@ -30,81 +30,88 @@ export default function IntegrationTypes() {
             >
               Integração Regional
             </h3>
-              
         </div>
         
         <div className="w-layout-grid grid-half">
-          <a href="#" className="grid-link w-inline-block">
-            <div className="grid-wrapper">
-            <div className="grid-image-wrapper">
-            <img 
-              src="https://i.pinimg.com/564x/e8/87/f0/e887f0eb96d2fa532955180419680dfb.jpg" loading="lazy" 
-              className="image-cover"
-            />
-          
-          </div>
-          <div className="grid-content-wrapper">
-            <h4 className="today-easter-font">Área de livre comércio</h4>
-          
-          </div>
-          
-          </div>
-          
-          </a>
-          
-          <a href="#" className="grid-link w-inline-block">
-            <div className="grid-wrapper">
-            <div className="grid-image-wrapper">
-            <img 
-              src="uniao-aduaneira.jpg" 
-              loading="lazy"
-              className="image-cover"
+          <Link href="/integration_types#integration-type-1">
+            <span className="grid-link w-inline-block">
+              <div className="grid-wrapper">
+              <div className="grid-image-wrapper">
+              <img 
+                src="https://i.pinimg.com/564x/e8/87/f0/e887f0eb96d2fa532955180419680dfb.jpg" loading="lazy" 
+                className="image-cover"
               />
-          
-          </div>
-          <div className="grid-content-wrapper">
-            <h4 className="today-easter-font">União Aduaneira</h4>
-          
-          </div>
-        
-          
-          </div>
-          
-          </a>
-          
-          <a href="#" className="grid-link w-inline-block">
-            <div className="grid-wrapper">
-              <div className="grid-image-wrapper">
-                <img 
-                  src="https://i.pinimg.com/564x/8b/24/1e/8b241e36c5968e12b3e6c2aac1534832.jpg" 
-                  loading="eager"
-                  className="image-cover"
-                />
-              
-              </div>
-              
-              <div className="grid-content-wrapper">
-                <h4 className="today-easter-font">Mercado Comum</h4>
-              </div>
+            
             </div>
-          </a>
-          
-          <a href="#" className="grid-link w-inline-block">
-            <div className="grid-wrapper">
+            <div className="grid-content-wrapper">
+              <h4 className="today-easter-font">Área de livre comércio</h4>
+            
+            </div>
+            
+            </div>
+            
+            </span>
+          </Link>
+         
+          <Link href="/integration_types#integration-type-2">
+            <span className="grid-link w-inline-block">
+              <div className="grid-wrapper">
               <div className="grid-image-wrapper">
-                <img 
-                  src="https://i.pinimg.com/564x/af/32/16/af3216d4d54884bbc4bb91afd67929ce.jpg" 
-                  loading="lazy" 
-                  className="image-cover"
+              <img 
+                src="uniao-aduaneira.jpg" 
+                loading="lazy"
+                className="image-cover"
                 />
             
-              </div>
-              
-              <div className="grid-content-wrapper">
-                <h4 className="today-easter-font">União Monetária</h4>
-              </div>
             </div>
-          </a>
+            <div className="grid-content-wrapper">
+              <h4 className="today-easter-font">União Aduaneira</h4>
+            
+            </div>
+          
+            
+            </div>
+            
+            </span>
+          </Link>
+         
+          <Link href="/integration_types#integration-type-3">
+            <span className="grid-link w-inline-block">
+              <div className="grid-wrapper">
+                <div className="grid-image-wrapper">
+                  <img 
+                    src="https://i.pinimg.com/564x/8b/24/1e/8b241e36c5968e12b3e6c2aac1534832.jpg" 
+                    loading="eager"
+                    className="image-cover"
+                  />
+                
+                </div>
+                
+                <div className="grid-content-wrapper">
+                  <h4 className="today-easter-font">Mercado Comum</h4>
+                </div>
+              </div>
+            </span>
+          </Link>
+         
+          <Link href="/integration_types#integration-type-4">
+            <span className="grid-link w-inline-block">
+              <div className="grid-wrapper">
+                <div className="grid-image-wrapper">
+                  <img 
+                    src="https://i.pinimg.com/564x/af/32/16/af3216d4d54884bbc4bb91afd67929ce.jpg" 
+                    loading="lazy" 
+                    className="image-cover"
+                  />
+              
+                </div>
+                
+                <div className="grid-content-wrapper">
+                  <h4 className="today-easter-font">União Monetária</h4>
+                </div>
+              </div>
+            </span>
+          </Link>
         </div>
       </div>    
     </section>
