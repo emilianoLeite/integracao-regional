bc975b7e7eb19e0886a8bf8cb868930001d9fbef
Emiliano Leite <emiliano.leite@creditas.com.br>
2021-09-30 09:36:35 -0300

    Finish Quiz1

--- components/quiz/question/question.js
+++ components/quiz/question/question.js
@@ -1,9 +1,20 @@
-import React from 'react';
 
-const Question = (props) => {
-    return (
-        <h4>{props.question}</h4>
-    );
+export default function Question({ title, subtitle }) {
+  return (
+    <>
+      <h4 
+        dangerouslySetInnerHTML={{__html: title}}
+      ></h4>
+      {subtitle ? 
+        <p
+          dangerouslySetInnerHTML={{__html: subtitle}}
+          style={{
+            maxWidth: "410px",
+            marginLeft: "58px",
+          }}
+        ></p> 
+        : null
+      }
+    </>
+  );
 }
-
-export default Question;
--- pages/quiz1/index.js
+++ pages/quiz1/index.js
@@ -2,37 +2,84 @@ import React, {Component} from 'react';
 import Question from '../../components/quiz/question/question';
 import Answer from '../../components/quiz/answer/answer';
 import styles from '../../components/quiz/quiz.module.css'
+import Link from 'next/link';
+
 
 export default class Quiz1 extends Component {
 
   // initiating the local state
   state = {
     questions: {
-      1: 'What US city is known as the "birthplace of jazz"?',
-      2: 'What is the capital of Greece?',
-      3: 'What planet gave birth to Superman?'
+      1: {
+        title: 'Identifique o <u>tipo de integração</u> descrita a seguir:',
+        subtitle: 'Voltada para um tema específico, como segurança, economia etc. Todos os países membros devem obrigatoriamente fazer parte de todas as negociações promovidas dentro da região. O intuito é facilitar transações para todos os envolvidos e, assim, fortalecer o grupo como um todo',
+      },
+      2: {
+        title: 'Assinale a alternativa com a explicação mais assertiva em relação ao <u>Regionalismo Aberto</u>:',
+        subtitle: null,
+      },
+      3: {
+        title: 'Sobre <u>Regionalização</u>, pode-se afirmar que:',
+        subtitle: null,
+      },
+      4: {
+        title: 'Leia o trecho a seguir:',
+        subtitle: '“Ao final da Guerra Fria, passou por um processo de reforma, e, após reconfigurar seu <i>modus operandi</i> e expandir sua linha de atuação se tornou uma organização voltada para a identificação e combate de ameaças, como o terrorismo e o narcotráfico”.',
+      },
+      5: {
+        title: 'Sobre os níveis de integração regional, assinale a <u>errada</u>:',
+        subtitle:null,
+      },
+      6: {
+        title: 'Sobre os níveis de integração regional, assinale a alternativa <u>incorreta</u>:',
+        subtitle:null,
+      },
     },
     answers: {
       1: {
-        1: 'Chicago',
-        2: 'New Orleans',
-        3: 'New York'
+        1: 'União Aduaneira',
+        2: 'União Monetária',
+        3: 'Mercado Comum',
+        4: 'Área de Livre Comércio',
       },
       2: {
-        1: 'Athens',
-        2: 'Patras',
-        3: 'Kalamata'
+        1: 'Concentração da região limitada à suas fronteiras. Seu objetivo é incentivar o desenvolvimento interno nos diversos setores produtivos, com foco na industrialização',
+        2: 'Inserção dos países ao comércio multilateral, abrindo a economia para a concorrência externa',
+        3: 'Região livre de taxação de produto e baixo nível de integração',
+        4: 'Instituições de segurança que lidam de forma diplomática com os conflitos internacionais, evitando o desencadeamento de guerras',
       },
       3: {
-        1: 'Krypton',
-        2: 'Mars',
-        3: 'Saturn'
-      }
+        1: 'É a ideia de olhar para processos que regionalizam países no âmbito macro, por meio da identificação de novos atores a uma região',
+        2: 'Movimento de fechamento regional, que tem por objetivo desenvolver as condições internas de competitividade e depois abrir o seu mercado interno para o comércio internacional',
+        3: 'Acordo pelo qual os países assumem riscos em relação a entrada facilitada de uma concorrência estrangeira. Exige maior esforço político de negociação',
+        4: 'É um conjunto de decisões que passam por ajustes e adequações políticas, visando a conformação de práticas comuns, com ganhos positivos coletivos e individuais',
+      },
+      4: {
+        1: 'OTAN',
+        2: 'ONU',
+        3: 'OMC',
+        4: 'União Europeia',
+      },
+      5: {
+        1: 'Trata-se de uma cooperação política, e a dimensão coletiva dos blocos resulta em redução da soberania',
+        2: 'Países entram em um acordo formal para maximizar seus ganhos',
+        3: 'Quanto maiores os esforços na tomada de decisão, o compromisso econômico e o comprometimento político dentro de uma integração, maior é o grau de aprofundamento da regionalização',
+        4: 'Seu objetivo é fortalecer as políticas públicas municipais voltadas para crianças, adolescentes, meio ambiente etc',
+      },
+      6: {
+        1: 'Uma área de livre comércio é uma região livre de taxação de produtos, e constitui um baixo nível de integração',
+        2: 'Em uma União Monetária, temos o nível máximo de integração, sendo a União Europeia a principal representante',
+        3: 'O Mercado Comum estabelece o livre fluxo de pessoas, bens e serviços',
+        4: 'A União Aduaneira envolve o livre comércio e tarifas igualitária somente para os países membros',
+      },
     },
     correctAnswers: {
-      1: '2',
-      2: '1',
-      3: '1'
+      1: '3',
+      2: '2',
+      3: '1',
+      4: '1',
+      5: '4',
+      6: '3',
     },
     correctAnswer: 0,
     clickedAnswer: 0,
@@ -76,7 +123,8 @@ export default class Quiz1 extends Component {
               (<div className="testimonial-card">
               <div className="testimonial-content">
                 <Question
-                  question={questions[step]}
+                  title={questions[step].title}
+                  subtitle={questions[step].subtitle}
                 />
                 <Answer
                   answer={answers[step]}
@@ -108,10 +156,31 @@ export default class Quiz1 extends Component {
               </div>
               </div>
               ) : (
-                <div className="finalPage">
-                  <h1>You have completed the quiz!</h1>
-                  <p>Your score is: {score} of {Object.keys(questions).length}</p>
-                  <p>Thank you!</p>
+                <div className="content-centered">
+                  <h1
+                    style={{
+                      textAlign: "center"
+                    }}
+                  >
+                    Você finalizou o teste!
+                  </h1>
+                  <p
+                    style={{
+                      fontSize: "2vw",
+                      textAlign: "center"
+                    }}
+                  >
+                    Sua pontuação é: {score} de {Object.keys(questions).length}
+                  </p>
+                  <Link href="/quiz1">
+                    <button 
+                      type="button" 
+                      className={`${styles.submitButton} ${styles.wButton} `}
+                      onClick={() => { document.location.reload()}}
+                    >
+                        Refazer
+                    </button>
+                  </Link>
                 </div>
               )
             }
